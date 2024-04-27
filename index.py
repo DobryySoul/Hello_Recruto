9361c50d63a28207b6c01c38414d226621f92810
Nikita <161777453+DobryySoul@users.noreply.github.com>
2024-04-27 10:44:18 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    name = request.args.get('name')
+    message = request.args.get('message')
+    try:
+        return f"Hello {name}! {message}!"
+    except:
+        return "ERROR"
+
+if __name__ == "__main__":
+    app.run()
